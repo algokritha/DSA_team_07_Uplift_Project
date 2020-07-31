4e4414227ca8d5c75c03c907df589e11cf4f9e06
Neel Shah <32770085+deadpanther@users.noreply.github.com>
2020-07-31 22:46:23 +0530

    Update number-of-burgers-with-no-waste-of-ingredients.cpp

--- Week 3/Neel/26_Sunday/number-of-burgers-with-no-waste-of-ingredients.cpp
+++ Week 3/Neel/26_Sunday/number-of-burgers-with-no-waste-of-ingredients.cpp
@@ -1,44 +1,10 @@
 class Solution {
 public:
     vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
-        int jumbo, small;
-        jumbo = 0;
-        small = 0;
-        vector<int> val;
-        if(tomatoSlices == 0 || cheeseSlices == 0){
-            val.push_back(0);
-            val.push_back(0);
-            return val;
-        }
-        int arr[cheeseSlices];
-        for(int i=0; i<cheeseSlices; i++){
-            arr[i] = 2;
-        }
-        long long total = cheeseSlices*2;
-        if(tomatoSlices%2 != 0 || total > tomatoSlices){
-            return val;
-        }
-        
-        int i=0;
-        while(i != cheeseSlices){
-            if(total < tomatoSlices){
-                arr[i] = 4;
-                total += 2;
-            }else if (total == cheeseSlices){
-                break;
-            }
-            i++;
-        }
-        for(int i=0; i<cheeseSlices; i++){
-            //cout<<arr[i]<<" ";
-            if(arr[i] == 4){
-                jumbo++;
-            }else{
-                small++;
-            }
-        }
-        val.push_back(jumbo);
-        val.push_back(small);
-        return val;
+    int x = tomatoSlices / 2 - cheeseSlices;
+    int y = cheeseSlices - x;
+    if (4 * x + 2 * y == tomatoSlices && x >= 0 && y >= 0) return {x, y};
+    return {};
+
     }
 };
