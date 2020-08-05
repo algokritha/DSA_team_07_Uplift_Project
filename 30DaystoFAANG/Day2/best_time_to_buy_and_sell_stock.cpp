f446db4d3903cdf3223e936d6af1852e394c10db
Neel Shah <32770085+deadpanther@users.noreply.github.com>
2020-08-06 01:46:51 +0530

    Update best_time_to_buy_and_sell_stock.cpp

--- 30DaystoFAANG/Day2/best_time_to_buy_and_sell_stock.cpp
+++ 30DaystoFAANG/Day2/best_time_to_buy_and_sell_stock.cpp
@@ -26,3 +26,19 @@ public:
         return max_profit;
     }
 };
+
+/* One pass solution */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = INT_MAX;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+};
