02aeda79b1f1a50ce75464c7627dfd7fabbc2f96
Neel Shah <32770085+deadpanther@users.noreply.github.com>
2020-08-06 00:59:19 +0530

    Create inversion_of_array_with_merge_sort.cpp

---
+++ 30DaystoFAANG/Day2/inversion_of_array_with_merge_sort.cpp
@@ -0,0 +1,66 @@
+/* Problem Link => https://practice.geeksforgeeks.org/problems/inversion-of-array/0 */
+
+#include <iostream>
+using namespace std;
+int counter = 0;
+void merge(int *array, int l, int m, int r) {
+   int i, j, k, nl, nr;
+   //size of left and right sub-arrays
+   nl = m-l+1; nr = r-m;
+   int larr[nl], rarr[nr];
+   //fill left and right sub-arrays
+   for(i = 0; i<nl; i++)
+      larr[i] = array[l+i];
+   for(j = 0; j<nr; j++)
+      rarr[j] = array[m+1+j];
+   i = 0; j = 0; k = l;
+   //merge temp arrays to real array
+   while(i < nl && j<nr) {
+      if(larr[i] <= rarr[j]) {
+         array[k] = larr[i];
+         i++;
+      }else{
+         array[k] = rarr[j];
+         counter += nl - i;
+         j++;
+      }
+      k++;
+   }
+   while(i<nl) {       //extra element in left array
+      array[k] = larr[i];
+      i++; k++;
+     
+   }
+   while(j<nr) {     //extra element in right array
+      array[k] = rarr[j];
+      j++; k++;
+     
+   }
+}
+
+void mergeSort(int *arr, int l, int r){
+    int m;
+    if(l < r){
+        m = l+(r-l)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0; i<n; i++){
+	        cin>>arr[i];
+	    }
+	    mergeSort(arr, 0, n-1);
+	    cout<<counter<<"\n";
+	}
+	return 0;
+}
