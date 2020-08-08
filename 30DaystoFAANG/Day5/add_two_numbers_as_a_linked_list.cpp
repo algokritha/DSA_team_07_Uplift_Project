727e051941c5b659a88c0ae941e058392acfa052
Neel Shah <32770085+deadpanther@users.noreply.github.com>
2020-08-09 00:17:58 +0530

    Update add_two_numbers_as_a_linked_list.cpp

--- 30DaystoFAANG/Day5/add_two_numbers_as_a_linked_list.cpp
+++ 30DaystoFAANG/Day5/add_two_numbers_as_a_linked_list.cpp
@@ -61,3 +61,64 @@ public:
         return head;
     }
 };
+
+/* Efficient but does not satisfy all estcases, needs work */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode(NULL);
+        ListNode *itr = new ListNode();
+        
+        itr = head;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL){
+            if(head == NULL){
+                head->val = l1->val + l2->val;
+                head->next = NULL;
+                itr = head;
+            }else{
+                ListNode *temp = new ListNode();
+                temp->val = l1->val + l2->val + carry;
+                if(carry == 1){
+                    carry = 0;
+                }
+                if(temp->val > 9){
+                    carry = 1;
+                    temp->val = temp->val%10;
+                }
+                temp->next = NULL;
+                itr->next = temp;
+                itr = itr->next;
+            }
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        if(l1 == NULL && l2 != NULL){
+            itr->next = l2;
+        }
+        if(l1 != NULL & l2 == NULL){
+            itr->next = l1;
+        }
+        cout<<carry;
+        if(carry == 1){
+            ListNode *temp = new ListNode();
+            temp->val = 1;
+            temp->next = NULL;
+            itr->next = temp;
+            itr = itr->next;
+        }
+        
+        return head->next;
+    }
+};
