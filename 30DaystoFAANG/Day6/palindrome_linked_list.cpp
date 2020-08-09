78a871896bcd4930e686eab6fb09e94430839aec
Neel Shah <32770085+deadpanther@users.noreply.github.com>
2020-08-09 17:55:46 +0530

    Update palindrome_linked_list.cpp

--- 30DaystoFAANG/Day6/palindrome_linked_list.cpp
+++ 30DaystoFAANG/Day6/palindrome_linked_list.cpp
@@ -1,5 +1,4 @@
 /* Problem Link => https://leetcode.com/problems/palindrome-linked-list/ */
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,52 +12,40 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode *itr = head;
-        int n = 0;
-        while(itr != NULL){
-            itr = itr->next;
-            n++;
+        ListNode *sp = head;
+        ListNode *fp = head;
+        ListNode *mid = NULL;
+        
+        while(fp != NULL && fp->next != NULL){
+            sp = sp->next;
+            fp = fp->next->next;
         }
         
-        if(n == 0 || n == 1){
-            return true;
+        if(fp != NULL){
+            mid = sp->next;
+        }else{
+            mid = sp;
         }
-
-        if(n == 2){
-            if(head->val == head->next->val){
-                return true;
-            }else{
+        
+        ListNode *prev = NULL;
+        ListNode *next = NULL;
+        
+        while(mid != NULL){
+            next = mid->next;
+            mid->next = prev;
+            prev = mid;
+            mid = next;
+        }
+        
+        while(prev != NULL){
+            if(prev->val != head->val){
                 return false;
+            }else{
+                prev = prev->next;
+                head = head->next;
             }
         }
-        
-        stack<int> s;
-        while(itr != NULL){ 
-            s.push(itr->val); 
-            // Move ahead  
-            itr = itr->next; 
-        } 
-  
-        // Iterate in the list again and  
-        // check by popping from the stack 
-        while(head != NULL){ 
-              
-            // Get the top most element  
-             int i=s.top(); 
-  
-             // Pop the element  
-             s.pop(); 
-  
-             // Check if data is not 
-             // same as popped element  
-            if(head->val != i){ 
-                return false; 
-            } 
-  
-            // Move ahead  
-           head=head->next; 
-        } 
-  
         return true;
+        
     }
 };
