bf328b64ed037bda6fc1384d2a01f50c48237cd2
Neel Shah <32770085+deadpanther@users.noreply.github.com>
2020-08-09 22:30:17 +0530

    Update rotate_linked_list.cpp

--- 30DaystoFAANG/Day6/rotate_linked_list.cpp
+++ 30DaystoFAANG/Day6/rotate_linked_list.cpp
@@ -37,3 +37,53 @@ public:
         return head;
     }
 };
+
+/* Efficient approach after a little modification */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL){
+            return NULL;
+        }
+        if(head->next == NULL){
+            return head;
+        }
+        int n = 1;
+        ListNode *tail = head;
+        while(tail->next != NULL){
+            tail = tail->next;
+            n++;
+        }
+        
+        //tail->next = head;
+        
+        int toChange = k%n;
+        
+        while(toChange--){
+            ListNode *tail = head;
+            ListNode *prev = head;
+            while(tail->next != NULL){
+                prev = tail;
+                tail = tail->next;
+            }
+            
+            prev->next = NULL;
+            tail->next = head;
+            head = tail;
+        }
+        cout<<n;
+        //cout<<tail->val;
+        return head;
+    }
+};
